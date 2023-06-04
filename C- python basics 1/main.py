073ae771949b0341fcd320dec06360609342da07
alirezafazeli8 <alirezafazeli34@gmail.com>
2023-06-04 10:53:26 +0430

    string section is completed

---
+++ C- python basics 1/main.py
@@ -0,0 +1,204 @@
+# # fandamental data types
+
+# int
+
+# float
+
+# bool
+
+# str
+
+# list
+
+# tuple
+
+# set
+
+# dict
+
+# # Classes -> custom data types
+
+
+# # specialized data types
+
+# None
+
+# print(2 + 2 * 8 / 9)
+
+
+# print(2 + 2)
+# print(2 / 2)
+# print(2 * 2)
+# print(2 - 5)
+
+
+# print(type("hi"))
+# print(type(5))
+# print(type(5.5))
+# print(type(True))
+
+
+# numberOne = 2
+# tavan = 2
+
+# print(numberOne ** tavan)
+
+# print(10 / 2)
+# print(10 // 2)
+
+# print(2 % 2)
+# print(5 % 3)
+# print(10 % 4.5)
+# print(10 / 4.5)
+
+
+# a = 3.78423423423634
+
+# print(round(a, 3))
+
+# print(round(3.4))
+# print(round(3.6))
+
+
+# print(abs(-20))
+# print(abs(+20))
+# print(abs(+20))
+# print(abs(--20))
+# print(abs(++20))
+
+
+# print((-20))
+# print((+20))
+# print((+20))
+# print((--20))
+# print((++20))
+
+
+# input('Whats your name ?')
+
+
+# userName = input('whats your name ? ')
+
+# print('Hello ' + userName)
+
+
+# print(2 ** 2)
+
+#  floor division
+
+# print(10 / 2.1)
+# print(10 // 2.1)
+
+
+# int1 = 3.2545467486
+# int2 = 5.75245456
+
+# print(round(int1, 2))
+# print(round(int2, 2))
+
+
+# print(round(int1))
+# print(round(int2))
+
+
+# int1 = -2434
+# int2 = +43432
+
+# # str3 = "2"
+
+
+# print(abs(int1))
+# print(abs(int2))
+# # print(abs(str3))
+
+
+# print(bin(5))
+
+
+# print(int('0b101', 2))
+
+
+# Snake Case
+# user_name = "alireza"
+# userName = "mohsen"
+# _userName = "sara"
+
+# print(user_name)
+# print(userName)
+# print(_userName)
+
+
+# print = "us"
+
+# print(print)
+
+
+# Constant Variable
+
+# USERNAME = "James"
+# print(USERNAME)
+
+# # dont overwrite the constant variable
+# USERNAME = "siamak"
+# print(USERNAME)
+
+
+# augmented assignment operator
+# some_value = 6
+# some_value = some_value + 2
+
+# print(some_value)
+
+
+# alternative of last code is :
+
+# some_value = 2
+
+# # some_value = some_value + 2
+# # some_value += 2
+# # some_value -= 2
+# # some_value *= 2
+# # some_value /= 2
+# # some_value %= 2
+
+# print(some_value)
+
+# print("string")
+# print('string')
+
+# print(type("hello"))
+
+# # long string
+
+# longString = '''
+
+# This is very long text
+
+# im from iran
+
+# '''
+
+# print(longString)
+
+
+# firstName = 'alireza'
+# lastName = 'fazeli'
+
+# fullName = firstName + lastName
+
+# print(fullName)
+
+
+# firstName = 'alireza'
+# lastName = 'fazeli' 
+
+# fullName = firstName + lastName
+
+# # خروجی  : alirezafazeli
+# # برای اینکه بتونیم متنمون رو خوشگل تر بکنیم میتونیم از متن های دیگه استفاده بکنیم : 
+
+# fullName = 'Name :' + ' ' + firstName + ' ' + 'last name :' + ' ' + lastName
+
+# print(fullName)
+
+# Type Conversion
