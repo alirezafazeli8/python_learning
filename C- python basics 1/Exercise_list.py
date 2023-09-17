022ec7ed4a8f96dc4c8daddc903d14393ab0459c
alirezafazeli8 <alirezafazeli34@gmail.com>
2023-09-17 15:26:35 +0330

    list Exercise is Done

---
+++ C- python basics 1/Exercise_list.py
@@ -0,0 +1,21 @@
+# using this list, 
+basket = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+# 1. Remove the Banana from the list
+basket.remove("Banana")
+# 2. Remove "Blueberries" from the list.
+basket.pop()
+# 3. Put "Kiwi" at the end of the list.
+basket.append("Kiwi")
+# 4. Add "Apples" at the beginning of the list
+basket.insert(0, "Apples")
+# 5. Count how many apples in the basket
+print(basket.count("Apples"))
+# 6. empty the basket
+basket.clear()
+
+print(basket)
+
+
+
+
