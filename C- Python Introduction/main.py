db25ab815f54b7cf169b1889ced93d65144d1e19
alirezafazeli8 <alirezafazeli34@gmail.com>
2024-01-30 15:31:27 +0330

    Lets bring all codes into JUPYTER NOTEBOOK 📝

--- C- Python Introduction/main.py
+++
@@ -1,4 +0,0 @@
-# print('Hello World')
-name = input("What is your name ? ")
-# print(name)
-print('Hello Mr/ms. ' + name)
--- C- python basics 1/Exercise_type_conversion.py
+++
@@ -1,5 +0,0 @@
-get_birth = input("Pleas Enter Your Birth day : ")
-age = 2023 - int(get_birth)
-say_age = f"Your age is : {age} :)"
-
-print(say_age)
