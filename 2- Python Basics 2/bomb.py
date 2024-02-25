2aa572611d02ac872a7a61aa8b6dc3ab00bfc2a0
alirezafazeli8 <alirezafazeli34@gmail.com>
2024-02-25 21:36:48 +0330

    Today Practice and Project is completed ✅

--- 2- Python Basics 2/bomb.py
+++ 2- Python Basics 2/bomb.py
@@ -11,19 +11,18 @@ computer_choice = random.choice(numbers)
 user_try = 5
 
 while True:
-
     # user input choice
     user_input = int(input("Guess Number between 1 and 10 :"))
-    
+
     if user_input == computer_choice:
-        print('God damn right choice 💣')
+        print("God damn right choice 💣")
         break
     elif user_try == 0:
         print("You Died 🥹")
         break
     elif user_input > computer_choice:
-        print(f'number is greater , chose less than. {user_try} time remain')
+        print(f"number is greater , chose less than. {user_try} time remain")
         user_try -= 1
     elif user_input < computer_choice:
-        print(f'number is less , chose greater than. {user_try} time remain')
+        print(f"number is less , chose greater than. {user_try} time remain")
         user_try -= 1
