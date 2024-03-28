e0a1b2c2005c55ce5727e0cda7be0c2f0cc6cd77
Alireza Fazeli <alirezafazeli34@gmail.com>
2024-03-28 09:26:35 +0330

    practice oop section is done

--- shit_code_practice/main.py
+++ shit_code_practice/main.py
@@ -22,6 +22,9 @@
 # print(f"c : {id(c)}")
 
 
+from typing import Any
+
+
 names = ["alireza", "sara", "mohsen", "javad"]
 user_info  = {
     "name": "alireza",
@@ -119,4 +122,138 @@ user_info  = {
 #     return y()
 
 # print(x())
-print("      a        ".strip())
+# print("      a        ".strip())
+
+# class Car:
+#     # class object attribute
+#     switch = True
+    
+#     def __init__(self, brand="BMW", model="AMG", color="BLACK", speed="250km"):
+#         self.brand = brand
+#         self.__model = model
+#         self.color = color
+#         # self.speed = speed
+#         Car.speed = speed
+        
+#     def start(self):
+#         if self.switch:
+#             return f"{self.brand}:{self.__model} is started"
+#         else: 
+#             return "switch is off"
+    
+#     # @classmethod
+#     # def can_drive(cls, age):
+#     #     if age >= 18:
+#     #         print("You can Drive with cars")
+#     #     else:
+#     #         print("You Cant Drive With Cars.")
+    
+#     @staticmethod
+#     def can_drive(age):
+#         if age >= 18:
+#             print("You can Drive with cars")
+#         else:
+#             print("You Cant Drive With Cars.")
+        
+
+# car_one = Car()
+
+# print(car_one.start())
+# print(car_one.speed)
+
+# Car.can_drive(15)
+
+
+class User:
+    def __init__(self, username, age):
+        if age >= 18:
+            self.username = username
+            self.age = age
+        else:
+            print("You Cant Play My Game.")
+    def sign_in(self):
+        print("User is Signed in.")
+        
+            
+class Wizard(User):
+    def __init__(self, username, wizard_name, ability):
+        User.username = username
+        self.wizard_name = wizard_name
+        self.ability = ability
+        
+    def sign_in(self):
+        super().sign_in()
+    
+        print(f"User {self.wizard_name} is signed in.")
+    
+    def attack(self):
+        return f"{self.wizard_name} attakced with {self.ability}"
+
+
+class Archer(User):
+    def __init__(self, archer_name, ability):
+        self.archer_name = archer_name
+        self.ability = ability
+        
+    def attack(self):
+        return f"{self.archer_name} attakced with {self.ability}"
+        
+        
+        
+class Jungler(Wizard, Archer):
+    def __init__(self, username, wizard_name, ability):
+        Archer.__init__(self, wizard_name, ability)
+        Wizard.__init__(self, username, wizard_name, ability)
+
+
+jungler_one = Jungler("alirezafazeli", "Jungler", "speaking")
+
+print(jungler_one.attack())
+
+
+# user_one = User("alirezafazeli", 18)
+# wizard = Wizard(user_one.username, "Rocky", "Fire")
+# # archer = Archer("Vexana", "arrows")
+
+# print(wizard.username)
+
+# print(dir(Wizard))
+
+# class Toy:
+    
+#     names = ["benz", "bmw", "volvo"]
+    
+#     def __init__(self, name, color):
+#         self.name = name
+#         self.color = color
+    
+#     def __str__(self):
+#         return "This is Specific Toy For Who Loves Car."
+    
+#     def __len__(self):
+#         return 100
+    
+#     # def __del__(self):
+#     #     print("Toy Is Deleted From Your Mind .")
+        
+#     def __call__(self, name):
+#         return f"Your {name} toy is created ."
+    
+#     def __getitem__(self, i):
+#         return self.names[i]
+        
+        
+# toy_car = Toy("Car Toy", "red")
+
+# # print(str(toy_car))
+# # print(len(toy_car))
+
+# # del toy_car
+
+# # print(Toy("knife", "black"))
+# print(toy_car("knife"))
+# print(toy_car[1])
+
+
+# print(issubclass(Toy, User))
+
