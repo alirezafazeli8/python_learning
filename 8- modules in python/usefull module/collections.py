bfb60c729273713d312ed700690c024ac20fa428
alirezafazeli8 <alirezafazeli34@gmail.com>
2024-09-21 22:24:09 +0330

    module section is over

---
+++ 8- modules in python/usefull module/collections.py
@@ -0,0 +1,9 @@
+from collections import Counter, OrderedDict, defaultdict
+
+
+names = ["reza", "ali", "reza", "sara", "javad", "sara", "sara"]
+
+sentence = "ja ja javad salam be be roye"
+
+print(Counter(names))
+print(Counter(sentence))
