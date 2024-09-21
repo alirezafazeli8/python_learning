0f7d9407521f327a9d213c5ee5f089190e41ce9f
alirezafazeli8 <alirezafazeli34@gmail.com>
2024-09-22 00:16:44 +0330

    read and write file with python section is done

---
+++ 10- File IO/main.py
@@ -0,0 +1,40 @@
+# my_file = open("./test.txt")
+
+# print(my_file.read())
+# my_file.seek(3)
+# print(my_file.read())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+
+# print(my_file.readlines())
+# my_file.close()
+
+# with open("./test.txt", mode="r+") as my_file:
+#     for i in range(10):
+#         my_file.write(f"{i + 1}\n")
+
+
+# with open("./test.txt", mode="a") as my_file:
+#     for i in range(20, 30):
+#         my_file.write(f"{i + 1}\n")
+
+
+# with open("./test.txt", mode="w") as my_file:
+#     for i in range(20, 30):
+#         my_file.write(f"{i + 1}\n")
+
+
+with open("./sad.txt", mode="w") as my_file:
+    for i in range(20, 30):
+        my_file.write(f"{i + 1}\n")
