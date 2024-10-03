044f23bf153fbe9ca87286097ddbad8fe7456206
alirezafazeli8 <alirezafazeli34@gmail.com>
2024-10-03 15:23:49 +0330

    unit test learning is done

--- 12- testing/test1/main.py
+++ 12- testing/test1/main.py
@@ -1,9 +1,16 @@
+# def do_stuff(num):
+#     if isinstance(num, int) and num >= 1:
+#         return num**2
+#     else:
+#         return "use real number"
+
+
+# def do_stuff_2(num):
+#     return num + 2
+
+
 def do_stuff(num):
-    if isinstance(num, int) and num >= 1:
-        return num**2
+    if num:
+        return num * 2
     else:
-        return "use real number"
-
-
-def do_stuff_2(num):
-    return num + 2
+        return "Pleas Enter Number"
--- 12- testing/test1/test.py
+++ 12- testing/test1/test.py
@@ -6,38 +6,77 @@ import main
 #     def test_do_stuff(self):
 #         test_param = 8
 #         result = main.do_stuff(test_param)
-#         self.assertEqual(result, 13)
+#         self.assertEqual(result, 64)
 
-#     def test_do_stuff2(self):
-#         test_param = "a"
-#         result = main.do_stuff(test_param)
-#         # self.assertEqual(result, test_param)
-#         self.assertTrue(isinstance(result, Error))
+# def test_do_stuff2(self):
+# test_param = "a"
+# result = main.do_stuff(test_param)
+# self.assertEqual(result, test_param)
+# self.assertRaises(AssertionError, main.do_stuff, "dasd")
 
 
 # unittest.main()
 
 
-class TestStuff(unittest.TestCase):
-    def test_number(self):
-        result = main.do_stuff(2)
-        self.assertEqual(result, 4)
+# class TestStuff(unittest.TestCase):
+#     def test_number(self):
+#         result = main.do_stuff(2)
+#         self.assertEqual(result, 4)
 
-    def test_negative(self):
-        result = main.do_stuff(-2)
-        self.assertTrue(result)
-        # self.assertEqual(result, -(2**2))
+#     def test_negative(self):
+#         result = main.do_stuff(-2)
+#         self.assertTrue(result)
+#         # self.assertEqual(result, -(2**2))
+
+#     def test_zero(self):
+#         result = main.do_stuff(0)
+#         self.assertTrue(result)
+#         # self.assertEqual(result, 0**2)
+
+
+# class TestStuff2(unittest.TestCase):
+#     def test_type(self):
+#         result = "a"
+#         self.assertRaises(TypeError, main.do_stuff_2, result)
+
+#     def test_instance(self):
+#         self.assertIsInstance("a", str)
+
+
+# unittest.main()
+
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        print("SetUp Method is running ...")
+
+    def test_do_stuff(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+
+        self.assertEqual(result, "Pleas Enter Number")
+
+    def test_empty_str(self):
+        test_param = ""
+        result = main.do_stuff(test_param)
+
+        self.assertEqual(result, "Pleas Enter Number")
 
     def test_zero(self):
-        result = main.do_stuff(0)
-        self.assertTrue(result)
-        # self.assertEqual(result, 0**2)
+        """
+        Test is Good
+        """
+        test_param = 0
+        result = main.do_stuff(test_param)
+
+        self.assertEqual(result, "Pleas Enter Number")
+
+    def tearDown(self):
+        print("Close()")
 
 
-class TestStuff2(unittest.TestCase):
-    def test_type(self):
-        result = "a"
-        self.assertRaises(TypeError, main.do_stuff_2, result)
-
+if __name__ == "__main__":
+    unittest.main()
 
 unittest.main()
