51baaf7d2223aace656bf05eaa23bd3b036b1d6b
alirezafazeli8 <alirezafazeli34@gmail.com>
2024-11-07 18:03:53 +0330

    pdf merger project is done

---
+++ --course_project--/pypdf/pdf_merger/pdf.py
@@ -0,0 +1,23 @@
+import sys
+from PyPDF2 import PdfMerger
+
+try:
+    # read terminal args
+    pdf_lists = [sys.argv[1], sys.argv[2]]
+
+    # create pdf merger object
+    pdf_merger = PdfMerger()
+
+    # add pdf's to pdf merger
+    for pdf in pdf_lists:
+        pdf_merger.append(pdf)
+
+    # create new pdf
+    pdf_merger.write("./assets/new_merge_pdf.pdf")
+    pdf_merger.close()
+
+except IndexError:
+    print("Pleas type to pdf's file path")
+# handle file path error
+except FileNotFoundError:
+    print("I cant find pdf file .")
