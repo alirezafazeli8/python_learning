0a60238cde54620e6012375ee214537f68a53fc6
alirezafazeli8 <alirezafazeli34@gmail.com>
2025-08-22 23:45:36 +0330

    lambda function added to shopping cart

--- 4- Functional Programming/shopping_cart.py
+++ 4- Functional Programming/shopping_cart.py
@@ -7,14 +7,19 @@ shopping_cart = [
     {"name": "milk", "price": "2", "count": "5"},
 ]
 
+# regular function
+# def calc_amount(acc, item):
+#     calc = int(item["price"]) * int(item["count"])
+#     acc += calc
+#     return acc
 
-def calc_amount(acc, item):
-    calc = int(item["price"]) * int(item["count"])
-    acc += calc
-    return acc
 
+# amount = reduce(calc_amount, shopping_cart, 0)
 
-amount = reduce(calc_amount, shopping_cart, 0)
+# with lambda function
+amount = reduce(
+    lambda acc, item: acc + (int(item["price"]) * int(item["count"])), shopping_cart, 0
+)
 
 
 print(f"Total Pay : {amount}$")
